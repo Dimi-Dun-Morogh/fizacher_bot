78d75a9aa569690845fc5e47bb14737279e30dab
dunmo <darnelbro@gmail.com>
2025-11-23 10:56:48 +0200

    fix db config

--- src/db/fireStoreInit.js
+++ src/db/fireStoreInit.js
@@ -1,14 +1,11 @@
 // Import the necessary modules from firebase-admin
 import  { initializeApp, cert } from 'firebase-admin/app';
 import  { getFirestore } from 'firebase-admin/firestore';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-
-import { readFileSync } from 'fs';
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
-const serviceAccount = JSON.parse(readFileSync( join(__dirname +'../../../service_account.json'), 'utf8'));
+
+
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
 initializeApp({
   credential: cert(serviceAccount),
